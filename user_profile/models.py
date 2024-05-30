67f3a6da9a960ef9d10476d84d9f3e6ff7f76146
Rasel006 <rizurasel.cse@gmail.com>
2024-05-30 22:36:15 +0600

    update user

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+SEX_CHOICES = [
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+]
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='user_profile/images/')
+    mobile_no = models.CharField(max_length = 12)
+    age = models.IntegerField(default=18)
+    portfolio = models.URLField(blank=True)
+    sex = models.CharField(choices = SEX_CHOICES, max_length = 10, default="Male")
+    bio = models.TextField(blank=True)
+    facebook = models.URLField(blank=True)
+
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
